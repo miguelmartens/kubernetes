b35c2511f0000b57dddeac245e9893728a9fb0c4
Miguel Martens <37029379+miguelmartens@users.noreply.github.com>
2021-06-14 23:52:47 +0200

    Create delete-rogue-namespace.sh

---
+++ scripts/delete-rogue-namespace.sh
@@ -0,0 +1,6 @@
+{
+NAMESPACE=<ROGUE-NAMESPACE>
+kubectl get namespace $NAMESPACE -o json \
+  | tr -d "\n" | sed "s/\"finalizers\": \[[^]]\+\]/\"finalizers\": []/" \
+  | kubectl replace --raw /api/v1/namespaces/$NAMESPACE/finalize -f -
+}
